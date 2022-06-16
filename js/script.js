a5828ddb68bdb357e826470fa31cf3c6f4db1f1e
ImraneAli <imrane.ali@etu.univ-amu.fr>
2022-06-16 12:09:03 +0200

    modifications de labels + gestion des notifications selon les températures

--- js/script.js
+++ js/script.js
@@ -36,11 +36,9 @@ onglets.forEach(onglet => {
 //capteurs
 
 if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker.register('/Hot-Hot-Hot-Project/service-worker.js') // à adapter à l'URL du projet
+    navigator.serviceWorker.register('/Hot-Hot-Hot-Project/service-worker.js')
 
     .then(() => { /*console.log('Service Worker Registered');*/ });
-
 }
 
 // Create WebSocket connection.
@@ -88,12 +86,6 @@ function drawChart() {
 
             console.log(latempExt + valTemp);
             console.log(latempInt + valTemp2);
-            if (valTemp > 18 || valTemp2 > 18) {
-                console.log("c'est l'été ou quoi ?");
-            }
-            if (valTemp2 < 5 || valTemp < 5) {
-                console.log("wesh le glaçon !");
-            }
 
             let tmp = document.querySelector(".temperature");
             tmp.textContent = latempExt;
@@ -158,12 +150,6 @@ function drawChart() {
                     let valTemp2 = interieur['Valeur'];
                     console.log(latempExt + valTemp);
                     console.log(latempInt + valTemp2);
-                    if (valTemp > 18 || valTemp2 > 18) {
-                        console.log("c'est l'été ou quoi ?");
-                    }
-                    if (valTemp2 < 5 || valTemp < 5) {
-                        console.log("wesh le glaçon !");
-                    }
 
                     let tmp = document.querySelector(".temperature");
                     tmp.textContent = parseInt(latempExt);
@@ -211,171 +197,167 @@ function drawChart() {
                     IndiceTemp = IndiceTemp + 1;
 
                     console.log("data bien reçu 2");
-
                 }))
         };
     }
 
 
     setInterval(() => {
-            if (socket.readyState != 1) {
-                const UrlApi = "https://hothothot.dog/api/capteurs";
-                fetch(UrlApi).then((response) => response.json().then((dataApi) => {
-                        let capteurs = dataApi;
-                        console.log(data);
-                        let ext = capteurs['capteurs'][1];
-                        let interieur = capteurs['capteurs'][0];
-                        var tempext = ext['Valeur'];
-                        var tempint = interieur['Valeur'];
-                        let latempExt = "Température extérieure : ";
-                        let valTemp = tempext;
-                        let latempInt = "Température intérieure : ";
-                        let valTemp2 = interieur['Valeur'];
+        if (socket.readyState != 1) {
+            const UrlApi = "https://hothothot.dog/api/capteurs";
+            fetch(UrlApi).then((response) => response.json().then((dataApi) => {
+                let capteurs = dataApi;
+                console.log(data);
+                let ext = capteurs['capteurs'][1];
+                let interieur = capteurs['capteurs'][0];
+                var tempext = ext['Valeur'];
+                var tempint = interieur['Valeur'];
+                let latempExt = "Température extérieure : ";
+                let valTemp = tempext;
+                let latempInt = "Température intérieure : ";
+                let valTemp2 = interieur['Valeur'];
 
-                        console.log(latempExt + valTemp);
-                        console.log(latempInt + valTemp2);
-                        /* notifs */
-                        let text1 = document.querySelector("#text1");
-                        let text2 = document.querySelector("#text2");
-                        let text3 = document.querySelector("#text3");
-                        let text4 = document.querySelector("#text4");
-                        let comptNotif = document.querySelector("#num");
-                        let notif1 = document.querySelector(".element-liste1");
-                        let notif2 = document.querySelector(".element-liste2");
-                        let notif3 = document.querySelector(".element-liste3");
-                        let notif4 = document.querySelector(".element-liste4");
-                        var comptN = 0;
-                        var comptN1 = 0;
-                        var comptN2 = 0;
-                        var comptN3 = 0;
-                        var comptN4 = 0;
+                console.log(latempExt + valTemp);
+                console.log(latempInt + valTemp2);
+                /* notifs */
+                let text1 = document.querySelector("#text1");
+                let text2 = document.querySelector("#text2");
+                let text3 = document.querySelector("#text3");
+                let text4 = document.querySelector("#text4");
+                let text5 = document.querySelector("#text5");
+                let text6 = document.querySelector("#text6");
+                let comptNotif = document.querySelector("#num");
+                let notif1 = document.querySelector(".element-liste1");
+                let notif2 = document.querySelector(".element-liste2");
+                let notif3 = document.querySelector(".element-liste3");
+                let notif4 = document.querySelector(".element-liste4");
+                let notif5 = document.querySelector(".element-liste5");
+                let notif6 = document.querySelector(".element-liste6");
+                var comptN1 = 0;
+                var comptN2 = 0;
+                var comptN3 = 0;
+                var comptN4 = 0;
+                var comptN5 = 0;
+                var comptN6 = 0;
 
-                        // notifier l'utilisateur en fonction des analyses de températures
-                        // il faudra factoriser pour implémenter le mot alerte devant le message
-                        if (valTemp > 35) {
-                            console.log("Hot Hot Hot !");
-                        }
+                // notifier l'utilisateur en fonction des analyses de températures
+                // il faudra factoriser pour implémenter le mot alerte devant le message
+                if (valTemp > 35) {
+                    console.log("Hot Hot Hot !");
 
-                        if (valTemp < 0) {
-                            console.log("Banquise en vue !");
-                        }
+                    text1.textContent = "alerte : Hot Hot Hot !";
+                    notif1.classList.add("element-liste1-active");
+                    var comptN1 = 1;
+                }
+                notif1.addEventListener("click", () => {
+                    notif1.classList.remove("element-liste1-active");
+                    notif1.classList.add("element-liste1");
+                });
 
-                        if (valTemp2 < 0) {
-                            console.log("Canalisations gelées, appelez SOS plombier et mettez un bonnet !");
-                        }
+                if (valTemp < 0) {
+                    console.log("Banquise en vue !");
 
-                        if (valTemp2 < 12) {
-                            console.log("Montez le chauffage ou mettez un gros pull !");
-                        }
+                    text2.textContent = "alerte : Banquise en vue !";
+                    notif2.classList.add("element-liste2-active");
+                    var comptN2 = 1;
+                }
+                notif2.addEventListener("click", () => {
+                    notif2.classList.remove("element-liste2-active");
+                    notif2.classList.add("element-liste2");
+                });
 
-                        if (valTemp2 > 22 && valTemp2 < 50) {
-                            console.log("Baissez le chauffage !");
-                        }
+                if (valTemp2 < 0) {
+                    console.log("Canalisations gelées, appelez SOS plombier et mettez un bonnet !");
 
-                        if (valTemp2 > 50) {
-                            console.log("Appelez les ompiers ou arrêtez votre barbecue !");
-                        }
+                    text3.textContent = "alerte : Canalisations gelées, appelez SOS plombier et mettez un bonnet !";
+                    notif3.classList.add("element-liste3-active");
+                    var comptN3 = 1;
+                }
+                notif3.addEventListener("click", () => {
+                    notif3.classList.remove("element-liste2-active");
+                    notif3.classList.add("element-liste3");
+                });
 
-                        notif1.classList.add("element-liste1-active");
-                        var comptN1 = 1;
-                    } else {
-                        text1.textContent = "il fait bon";
-                    }
-                    notif1.addEventListener("click", () => {
-                        notif1.classList.remove("element-liste1-active");
-                        notif1.classList.add("element-liste1");
-                    });
+                if (valTemp2 < 12) {
+                    console.log("Montez le chauffage ou mettez un gros pull !");
 
-                    if (valTemp > 22 || valTemp2 > 22) {
-                        messageTest = "il fait chaud dehors : " + valTemp + "°";
-                        messageTest1 = "il fait " + valTemp2 + "° à l'intérieur";
-                        console.log(messageTest);
+                    text4.textContent = "alerte : Montez le chauffage ou mettez un gros pull !";
+                    notif4.classList.add("element-liste4-active");
+                    var comptN4 = 1;
+                }
+                notif4.addEventListener("click", () => {
+                    notif4.classList.remove("element-liste4-active");
+                    notif4.classList.add("element-liste4");
+                });
 
-                        if (valTemp > 22 && valTemp2 < 22) {
-                            text2.textContent = messageTest;
-                        } else if (valTemp < 22 && valTemp2 > 22) {
-                            text2.textContent = messageTest1;
-                        } else if (valTemp > 22 && valTemp2 > 22) {
-                            text2.textContent = "il fait chaud aujourd'hui";
-                        }
 
-                        notif2.classList.add("element-liste2-active");
-                        var comptN2 = 1;
-                    } else {
-                        text2.textContent = "il fait chaud";
-                    }
-                    notif2.addEventListener("click", () => {
-                        notif2.classList.remove("element-liste2-active");
-                        notif2.classList.add("element-liste2");
-                    });
+                if (valTemp2 > 22 && valTemp2 < 50) {
+                    console.log("Baissez le chauffage !");
 
-                    if (valTemp2 < 10 || valTemp < 10) { /* 10 */
-                        console.log("il fait froid aujourd'hui, moins de 12°");
+                    text5.textContent = "alerte : Baissez le chauffage !";
+                    notif5.classList.add("element-liste5-active");
+                    var comptN5 = 1;
+                }
+                notif5.addEventListener("click", () => {
+                    notif5.classList.remove("element-liste5-active");
+                    notif5.classList.add("element-liste4");
+                });
 
-                        if (valTemp < 10 && valTemp2 > 10) {
-                            text3.textContent = "il fait froid dehors";
-                        } else if (valTemp > 10 && valTemp2 < 10) {
-                            text3.textContent = "il fait froid à l'intérieur";
-                        } else if (valTemp < 10 && valTemp2 < 10) {
-                            text3.textContent = "il fait froid";
-                        }
+                if (valTemp2 > 50) {
+                    console.log("Appelez les ompiers ou arrêtez votre barbecue !");
 
-                        notif3.classList.add("element-liste3-active");
-                        var comptN3 = 1;
-                    } else {
-                        text3.textContent = "il fait froid";
-                    }
-                    notif3.addEventListener("click", () => {
-                        notif3.classList.remove("element-liste3-active");
-                        notif3.classList.add("element-liste3");
-                    });
+                    text6.textContent = "alerte : Appelez les ompiers ou arrêtez votre barbecue !";
+                    notif6.classList.add("element-liste6-active");
+                    var comptN6 = 1;
+                }
+                notif6.addEventListener("click", () => {
+                    notif6.classList.remove("element-liste4-active");
+                    notif6.classList.add("element-liste6");
+                });
 
-                    if (valTemp2 < 5 || valTemp < 5) {
-                        console.log("il fait moins de 5°");
+                /* affichage de la température (englet 1) */
+                let tmp = document.querySelector(".temperature");
+                tmp.textContent = latempExt;
+                let valeurTemperature = document.querySelector(".la-temp");
+                valeurTemperature.textContent = valTemp;
 
-                        if (valTemp < 5 && valTemp2 > 5) {
-                            text4.textContent = "il fait moins de 5° dehors";
-                        } else if (valTemp > 5 && valTemp2 < 5) {
-                            text4.textContent = "allumez le chauffage";
-                        } else if (valTemp < 5 && valTemp2 < 5) {
-                            text4.textContent = "c'est glacial";
-                        }
+                let tmp2 = document.querySelector(".temperature2");
+                tmp2.textContent = latempInt;
+                let valeurTemperature2 = document.querySelector(".la-temp2");
+                valeurTemperature2.textContent = valTemp2;
 
-                        notif4.classList.add("element-liste4-active");
-                        var comptN4 = 1;
-                    } else {
-                        text4.textContent = "c'est glacial";
-                    }
-                    notif4.addEventListener("click", () => {
-                        notif4.classList.remove("element-liste4-active");
-                        notif4.classList.add("element-liste4");
-                    }); valeurFinale = comptN + comptN1 + comptN2 + comptN3 + comptN4; comptNotif.textContent = valeurFinale;
+                /* affichage de la température (graphe) */
+                var tabTemp = [];
+                var tmpsAct = new Date();
+                heure = tmpsAct.getHours();
+                minute = tmpsAct.getMinutes();
+                heure = heure < 10 ? "0" + heure : heure;
+                minute = minute < 10 ? "0" + minute : minute;
+                heureNminute = heure + "h : " + minute + "m";
+                tempext = JSON.parse(tempext);
+                tempint = JSON.parse(tempint);
+                tabTemp.push(heureNminute);
+                tabTemp.push(tempext);
+                tabTemp.push(tempint);
+                console.log(heureNminute);
+                console.log(tabTemp);
+                data.addRows([
+                    [tabTemp[0], tabTemp[1], tabTemp[2]]
+                ]);
+                if (numberOfData >= 40) {
+                    numberOfData = 38;
+                    data.removeRow(0);
+                    data.removeRow(1);
+                } else {
+                    numberOfData = numberOfData + 1;
+                }
 
-                    /* affichage de la température (englet 1) */
-                    let tmp = document.querySelector(".temperature"); tmp.textContent = latempExt;
-                    let valeurTemperature = document.querySelector(".la-temp"); valeurTemperature.textContent = valTemp;
+                var chart = new google.visualization.LineChart(document.querySelector('.curve_chart'));
+                chart.draw(data, options);
 
-                    let tmp2 = document.querySelector(".temperature2"); tmp2.textContent = latempInt;
-                    let valeurTemperature2 = document.querySelector(".la-temp2"); valeurTemperature2.textContent = valTemp2;
+                console.log("data bien reçu 3");
 
-                    /* affichage de la température (graphe) */
-                    var tabTemp = [];
-                    var tmpsAct = new Date(); heure = tmpsAct.getHours(); minute = tmpsAct.getMinutes(); heure = heure < 10 ? "0" + heure : heure; minute = minute < 10 ? "0" + minute : minute; heureNminute = heure + "h : " + minute + "m"; tempext = JSON.parse(tempext); tempint = JSON.parse(tempint); tabTemp.push(heureNminute); tabTemp.push(tempext); tabTemp.push(tempint); console.log(heureNminute); console.log(tabTemp); data.addRows([
-                        [tabTemp[0], tabTemp[1], tabTemp[2]]
-                    ]);
-                    if (numberOfData >= 40) {
-                        numberOfData = 38;
-                        data.removeRow(0);
-                        data.removeRow(1);
-                    } else {
-                        numberOfData = numberOfData + 1;
-                    }
-
-                    var chart = new google.visualization.LineChart(document.querySelector('.curve_chart')); chart.draw(data, options);
-
-                    console.log("data bien reçu 3");
-
-                }))
+            }))
         }
     }, 10000) /* 60000 */
 }
@@ -387,6 +369,8 @@ let element1 = document.querySelectorAll(".element-liste1");
 let element2 = document.querySelectorAll(".element-liste2");
 let element3 = document.querySelectorAll(".element-liste3");
 let element4 = document.querySelectorAll(".element-liste4");
+let element5 = document.querySelectorAll(".element-liste5");
+let element6 = document.querySelectorAll(".element-liste6");
 
 element1.forEach(elements => {
     elements.addEventListener("click", () => {
